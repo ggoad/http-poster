9d29650c7dfddeee4fa153d696342274eca5ee4e
Greg Goad <ggoadmusic@gmail.com>
2024-07-11 18:17:56 -0400

    Changed the config initialization to be a much mor strait-forward rename operation

--- cloneConfInit.php
+++ cloneConfInit.php
@@ -1,14 +1,5 @@
 <?php 
 $baseDir="";
-$dir=$baseDir."src/sampleConf";
-$tdir=$baseDir."src/conf";
-@mkdir($tdir);
-$sd=scandir($dir);
-$sd=array_filter($sd, function($s){
-	return preg_match('/\.json$/',$s);
-});
-foreach($sd as $s)
-{
-	copy("$dir/$s", "$tdir/$s");
-}
-echo "Copy Complete";
+rename($baseDir."src/sampleConf", $baseDir."src/conf");
+echo "Init Complete";
+
