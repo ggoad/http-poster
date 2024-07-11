691988eae3d8bf8cfb8198408a3f68c4f45cf04b
Greg Goad <ggoadmusic@gmail.com>
2024-07-11 16:44:37 -0400

    moved the configurations to a sample configuration

---
+++ cloneConfInit.php
@@ -0,0 +1,14 @@
+<?php 
+$baseDir="";
+$dir=$baseDir."src/sampleConf";
+$tdir=$baseDir."/src/conf";
+@mkdir($tdir);
+$sd=scandir($dir);
+$sd=array_filter($sd, function($s){
+	return preg_match('/\.json$/',$s);
+});
+foreach($sd as $s)
+{
+	copy("$dir/$s", "$tdir/$s");
+}
+echo "Copy Complete";
